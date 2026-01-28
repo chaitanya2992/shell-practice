db8216937d1b5a4c6f49d0b5de7a6323dce02387
chaitanya <vchaitanyapraveenreddy@gmail.com>
2026-01-28 12:44:18 +0530

    second commit with a convo

---
+++ 2ndscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+client="john"
+manager="jay"
+echo "$client:: what is the status of project $manager"
+echo "$manager:: going according with sprint $client"
+echo "$client:: heard about everything is set to release."
+echo "$manager:: yes! $client"
